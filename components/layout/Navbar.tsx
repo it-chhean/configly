6e9093fe3d2251181757611a1104388dd783d207
it-chhean <bchhean8@gmail.com>
2026-08-13 15:17:21 +0700

    chore: remove logo icon

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -6,7 +6,6 @@ const Navbar = () => {
     <header className="border-b ">
       <nav className='mx-auto flex h-14 max-w-5xl items-center justify-between px-6 '>
         <div>
-        <img src="/favicon.ico" alt="Configly Logo" className="h-7 w-7 mt-[-4px] mr-1 inline-block"/>
         <ArrowLink href="/" name="Configly"/>
         </div>
         <div className='flex items-center sm:flex gap-6 cursor-pointer'>
