9033194bd69c0e2bb7736eb331df71630f31843f
it-chhean <bchhean8@gmail.com>
2026-08-23 20:47:41 +0700

    make some changed.

--- app/layout.tsx
+++ app/layout.tsx
@@ -106,9 +106,6 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-    <head>
-      <link rel="icon"  href="/favicon.ico"/>
-    </head>
     <body className="h-screen scroll-smooth">
       <Navbar/>
         {children}
